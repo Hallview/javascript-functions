376ec75ce5b7b68c0d1af2ce4df117420be01d96
Adisa Halimat <halimatadisa@gmail.com>
2023-09-17 11:06:41 +0100

    assignment completed

---
+++ index.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+  
+console.log(add(2, 4));
+console.log(multiply(2,4))
+  
---
+++ readme.md
@@ -0,0 +1,4 @@
+Assignment
+
+1. Write a function to add two numbers
+2. Write a function to multiply two numbers
